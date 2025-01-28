f0394223805b7f7576ceb376e9d42573276c0fd9
Ahmed Rizk <ahmed.mostafa.rezk2001@gmail.com>
2025-01-28 05:37:55 +0200

    -fixing security issues related to firebase keys

--- lib/core/helpers/send_notifications/firebase_cloud_messaging.dart
+++ lib/core/helpers/send_notifications/firebase_cloud_messaging.dart
@@ -1,3 +1,4 @@
+import 'package:astroo_store_app/core/helpers/send_notifications/secrets/firebase_secrets.dart';
 import 'package:astroo_store_app/features/admin/admin_notifications/data/models/notification_model.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -63,23 +64,6 @@ class FirebaseCloudMessaging {
   }
 
   static Future<String?> getAccessToken() async {
-    final serviceAccountJson = {
-      "type": "service_account",
-      "project_id": "astroo-store-d14e1",
-      "private_key_id": "7810581273007dfa9992e359ea51cb8f3ffb26cd",
-      "private_key":
-          "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCvLooQMeLUn8Gf\nBV1xRUW1mCQf/rBtYW/px8gjxxGNWexC0ufLQWj8XsR7oXCY8nkUkS5iDQbMO7wT\nVsy2OeXsovfDkgX/6jydpAE3GbJo0jZiRRyDySCRwz2q2hlP1m1KnTwKoCh1bm0A\nUfDgpMyfb8zoPaBf18lYznuI2+P//RJ2uykGL2V/So/5A0sqa9gKV4cGJ9bH0nR+\ntS5Z/whl44yQxaMquiRliu4CiCx3m/5SUh5rbBaEOINswxc+rSNgeSJ3mVsOKzqc\nPRjBlZR+hLIwX5JTu/sJCNDhtvKiSBF6t1pn82InQ4oLGgrnr0fadsk+mMvDkOXX\nHTPNFOMXAgMBAAECggEAT7RDCN9JrygItkuAmlrEPr08InqE1x9wBdPqEa0vJTr2\nrei7odTe6YRMrVtkmZIo2Ny9V8tJfn7nMX1aosZxAFudkxPLOwbeEXNfQiyk6eBK\n8icztBo8gVVtp2CYB6UuELa9bLA1+EgHvA+MIwmYOCpr9najkzfg2lEID9rMP9mq\nXpMoPDq2D9bmKrhR0asSwVKg/PwuZT+/6K3l9g6u+nSwJbvsA1bIBFf4z1dd3Q1j\nSnA4sSocSDaB8hcqXYmHs5T/+M+adyIg5FqyfAegNf62XxFiQcons5E3QTfJMDFY\nf9uyf1Hoq+qBnWGFkajQbp85MVAfrgZDm3heowxLgQKBgQDYd5uhAApWGz2FmhJM\nw7uVkIL2vlsDpu06jO1PXR+WzPuxckeEg5XnFB6GkwHnThFShp/10Ik31cdJcGlV\n68bzLJ+h8qnyreAZTA4V80oI8xLAQIN9CmySV8Zzi3gwcm7CpZFqoI8SkHqvMo6u\nVzJyKWJl5hWO3nYpK8Ot9THPNwKBgQDPLLukQhR83gqejvbK+X3TRi+Ml5DVy3yG\nm8p1VgStqxNGmtxFBNYDxNiNoSWNvWOWWc5ThSzl6FXoBjJ24djnk9iCjAws0kS1\nQMSLaShtvUi7XrMVwc/D7GenEJL6XrziXwulIOZhSyvZSMfHEKFbN4wjml5hd7E+\nFpTu2RS7IQKBgHrk1Oc/1Z5zEqeCBDeuvBW6mI+rHJLjNzWRgr0W+nMr5w0j+zRh\nXD53rqjqVx9RtAXtRm1ROkYHyX+JIVRVanrub6aK1S9gxaS3CBwwJm0USyX4um3H\n+EHRMWqx2MdwgC9jvj4R74gznGNQya5y9MHZevKtALo+1Eilfw+I8AFbAoGAAKHa\nUL4C4f6reaBmEGiCgcqNCpjL0Fs2nhKLo6AHk6AaBvUiBvPk004eexAq1Svmyeas\nQoT9hhYXbQXX0ZUO3GYPGxbiUqCxpPv1nZdMkEkrl6bKVR6qS6bFCjV6vVjcqrQv\njOl0yoc4MwmN0oWyeFzLCLDH6PQ8jjvzaGcxMQECgYEAosYXgG6JSimDjJleqMb/\nscC+rmXJRGEHV94MkIPfXx1+ju9/ljhulMRupoCWRtnYqw2aZ7l2UWCp+k86R5md\n27ljGEyX7zNN9dqiw7uwQ/tmn8o1d/qDhbFDgL9Ev30zdWTJN12cj0/3hpMfoc//\nQKgyiB0dkmX6+5qL/kPGylc=\n-----END PRIVATE KEY-----\n",
-      "client_email":
-          "firebase-adminsdk-lccn6@astroo-store-d14e1.iam.gserviceaccount.com",
-      "client_id": "111896677320185043453",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url":
-          "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url":
-          "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-lccn6%40astroo-store-d14e1.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
-    };
     List<String> scopes = [
       "https://www.googleapis.com/auth/userinfo.email",
       "https://www.googleapis.com/auth/firebase.database",
@@ -111,10 +95,6 @@ class FirebaseCloudMessaging {
   }) async {
     try {
       var serverKeyAuthorization = await getAccessToken();
-
-      // change your project id
-      const String urlEndPoint =
-          "https://fcm.googleapis.com/v1/projects/astroo-store-d14e1/messages:send";
 
       Dio dio = Dio();
       dio.options.headers['Content-Type'] = 'application/json';
